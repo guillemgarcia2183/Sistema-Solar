9819f980cddcbc00aa6acdf6fb7e104e6e753cd1
WorthlessProgrammer <95526357+WorthlessProgrammer@users.noreply.github.com>
2024-10-23 17:49:30 +0200

    Implemented circular stars with mgl points and did m_model transforms

--- codi/star.py
+++ codi/star.py
@@ -21,6 +21,11 @@ class Star:
         self.y = y
         self.z = z
 
+        self.position = glm.vec3(self.x, self.y, self.z)
+        self.color = glm.vec3(1.0, 1.0, 1.0)
+
+        self.normal = glm.vec3(0, 0, 1)
+
         self.vbo = self.get_vbo()
         self.shader = self.get_shader_program()
         self.vao = self.get_vao()
@@ -33,13 +38,23 @@ class Star:
         self.shader['m_proj'].write(self.app.camera.m_proj)
         self.shader['m_view'].write(self.app.camera.m_view)
         self.shader['m_model'].write(self.m_model)
+        self.shader['star_color'].write(self.color)
 
     def get_model_matrix(self):
         # TODO: From X+ Y+ Z+ -> x, y, z
-        # CB -> v from V to u from U -> vU=u
+        # NOTE: Order of operations is from last -> first
+
+        # in-world placement of the star
         rads = glm.radians(23.44)
+        #m_model *= glm.translate(self.position) #TODO: Is this correct?
         m_model = glm.translate(-self.app.objects[2].position)
         m_model *= glm.rotate(glm.mat4(), rads, glm.vec3(0, 0, 1))
+
+        # make it look like a square
+        m_model *= glm.translate(self.position)  
+        m_model *= glm.rotate(glm.mat4(), glm.acos(glm.dot(glm.normalize(self.normal), glm.normalize(self.app.camera.position))), glm.vec3(0, 1, 0))
+        m_model *= glm.translate(-self.position) #first
+
         return m_model
 
     def get_vbo(self):
@@ -49,7 +64,7 @@ class Star:
 
     def render(self):
         self.ctx.enable(mgl.PROGRAM_POINT_SIZE)
-        self.ctx.point_size = 5
+        self.ctx.point_size = 10
         self.vao.render(mgl.POINTS)
 
     def destroy(self):
@@ -62,8 +77,9 @@ class Star:
         return vao
 
     def get_data(self):
-        
-        data = np.array([self.x, self.y, self.z], dtype='f4')
+        data = np.array([
+            self.x, self.y, self.z,
+        ], dtype='f4')
         return data
 
     def get_shader_program(self):
